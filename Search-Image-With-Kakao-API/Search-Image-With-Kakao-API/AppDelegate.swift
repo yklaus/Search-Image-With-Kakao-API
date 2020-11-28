4011819a139a99d35d4a0f2d8ad2d1830a3a0239
Sung Yu <hwchon1234@gmail.com>
2020-11-29 04:12:43 +0900

    Init Project

---
+++ Search-Image-With-Kakao-API/Search-Image-With-Kakao-API/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Search-Image-With-Kakao-API
+//
+//  Created by klaus on 2020/11/29.
+//  Copyright © 2020 yklaus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
